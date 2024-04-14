e0f5e5c967aec8cac9b4ab53e423a6d79e303189
Mattias Axelsson <yhma23.axelma@folkuniversitetet.nu>
2024-04-14 20:34:33 +0200

    Initial Commit

---
+++ Usertable/UsertableApp.swift
@@ -0,0 +1,17 @@
+//
+//  UsertableApp.swift
+//  Usertable
+//
+//  Created by Mattias Axelsson on 2024-04-14.
+//
+
+import SwiftUI
+
+@main
+struct UsertableApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
